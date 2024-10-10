ddee11523c3ea82669897622680e4a132ebdd9df
Avnoor Kaur <Avnoork2004@gmail.com>
2024-10-10 01:01:43 -0400

    3 questions answered

--- src/main/java/org/example/ArraysAndStreams2.java
+++ src/main/java/org/example/ArraysAndStreams2.java
@@ -34,5 +34,33 @@ public class ArraysAndStreams2 {
                         .collect(Collectors.toList()));
 
 
+
+        // Edited version
+
+        // #1
+        //Used stream to filter strings that start with a vowel (case-insensitive)
+        System.out.printf("strings start with a vowel (case-insensitive) : %s%n",
+                Arrays.stream(strings)   // Created the stream of strings
+                        .filter(s -> s.matches("(?i)^[aeiou].*")) // matched the strings starting with a vowel (case-insensitive)
+                        .collect(Collectors.toList()));    // Collected the matching strings into a list and then printed it
+
+
+        // #2
+        //Used a stream to concatenate all the strings into a single string, separated by a comma
+        System.out.printf("concatenated strings: %s%n",
+                Arrays.stream(strings)  // Created the stream of strings
+                        .collect(Collectors.joining(", "))); // Joined the strings in stream with  comma separator and printed it
+
+
+
+        // #3
+        //Used a stream to count the number of strings that contain more than 5 characters (case-insensitive)
+        long count = Arrays.stream(strings)    // Created the stream of strings
+                .filter(s -> s.length() > 5)   // Filtered the strings more than 5 characters
+                .count();                      // Counted number of strings that would match the condition
+        System.out.printf("number of strings that contain more than 5 characters (case-insensitive) : %d%n", count); // Printed the count
+
+
+
     }
 }
